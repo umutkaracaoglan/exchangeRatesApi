4318fa2d6b1dee3c57281d985772f5725c151a8f
umutkaracaoglan <umumtkaracaoglan@gmail.com>
2023-01-15 17:52:21 +0300

    add UserActivityLogs model & migrate

---
+++ database/migrations/2023_01_14_224903_create_user_activity_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_activity_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('endpoint');
+            $table->string('http_status');
+            $table->timestamp('called_at');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_activity_logs');
+    }
+};
