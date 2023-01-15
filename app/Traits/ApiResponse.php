ac8274943d5d7d98c46616b1d5f345daaaa44ac1
umutkaracaoglan <umumtkaracaoglan@gmail.com>
2023-01-15 19:53:43 +0300

    add api error handling - ApiResponse trait - ApiController controller

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponse
+{
+    protected function success(string $data, mixed $message = null, int $code = 200)
+    {
+        return response()->json([
+            'status'  => 'success',
+            'message' => $message,
+            'data'    => $data
+        ], $code);
+    }
+
+    protected function error(int $code, mixed $message = null)
+    {
+        return response()->json([
+            'status'  => 'error',
+            'message' => $message,
+            'data'    => null
+        ], $code);
+    }
+}
