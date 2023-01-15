cb543eac00d4dd04934b7718ad8348288f4cd8de
umutkaracaoglan <umumtkaracaoglan@gmail.com>
2023-01-15 17:50:59 +0300

    add exchangeRates model & migrate

---
+++ app/Models/ExchangeRates.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExchangeRates extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'code', 'rate_buy', 'rate_sell'
+    ];
+}
---
+++ database/migrations/2023_01_14_224914_create_exchange_rates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchange_rates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->unique();
+            $table->string('code', 6)->unique();
+            $table->float('rate_buy')->nullable();
+            $table->float('rate_sell')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exchange_rates');
+    }
+};
