b87bceae17a34e01c442d40a8d3180a6d267431c
umutkaracaoglan <umumtkaracaoglan@gmail.com>
2023-01-16 09:06:35 +0300

    Add api welcome message

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
+Route::get('/', function () {
+    return response()->json([
+        'status'  => 'success',
+        'message' => 'Welcome to exchange rates api!',
+    ], 200);
+    //return view('welcome');
+});
